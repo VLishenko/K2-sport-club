8729a7cb84690cf8b76f9f6ff57293ed569e41f1
Lishenko Vitaliy <lishenkovitaliy@gmail.com>
2016-09-30 22:31:13 +0300

    First Part of the site

--- js/common.js
+++ js/common.js
@@ -1,3 +1,39 @@
-$(function () {
-	// $('body').fadeOut();
+//Droped Menu
+$(function(){
+	 $('.hidden-navigation a').click(function(){
+	 	$('#hidden-navigation-list').slideToggle('fast');
+	 	return false;
+	 });
+});
+
+//Click out of Droped Menu
+$(document).mouseup(function (e) {
+  var link = $('.hidden-navigation a'),
+  		list = $('#hidden-navigation-list');
+
+  if (e.target != link [0]
+ 	&& link.has(e.target).length === 0
+  ){
+    list.slideUp();
+  }
+});
+
+//Turn off menu links
+$('.hidden-navigation-list a').click(function() {
+	return false;
+})
+
+//Slider
+$('.slider').slick({
+	dots: false,
+	arrows: false,
+  infinite: true,
+  speed: 500,
+	responsive: [
+	{
+	  breakpoint: 992,
+	  settings: {
+	  	arrows: true,
+	  }
+	}]
 });
